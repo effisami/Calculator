a48b7fd90d107e87ca2c443ac3eaaa16af370c3a
Samira Ghorbani <sghorbano@gmail.com>
2019-07-18 21:21:02 +0200

    Fixar CE button

--- src/App.js
+++ src/App.js
@@ -1,120 +1,89 @@
 import React from 'react';
 import './App.css';
-//import Plus from './plus';
-import './App.css';
-
-
-
 class App extends React.Component {
-state={
-  inputValue: '',
-  storedValue:'',
-  result : null,
-  operator : '+',
-  selectedButton :''
-}
-
-
-changeInputValue = (event) => {
-  this.setState({inputValue: event.target.value})
-}
-deleteRow = (event) => {
-  this.setState({result     : event.target.value})
-}
-
-plus = () => {
-
-  let newResult = this.state.result + Number(this.state.inputValue)
-  this.setState({
-   result: newResult,
+  state = {
+    inputValue: '',
+    storedValue: '',
+    result: null,
     operator: '+',
-    inputValue: ''
-  })
-}
-minus = () => {
-  let newResult
-  if(this.state.result === null) {
-    newResult = Number(this.state.inputValue)
+    selectedButton: ''
   }
-  else{
-  newResult = this.state.result - Number(this.state.inputValue)
+
+  changeInputValue = (event) => {
+    this.setState({ inputValue: event.target.value })
   }
-  this.setState({
-    result: newResult,
-    operator: '-',
-    inputValue: ''
-  })
-}
-likamed = () => {
-  let newResult;
-  let latestOperator = this.state.operator
-  if(latestOperator === '+'){
-  newResult = this.state.result + Number(this.state.inputValue)
+
+  deleteRow = (event) => {
+    this.setState({
+      result: null,
+      storedValue: '',
+      operator: '+',
+      inputValue: ''
+    })
   }
-  else if( latestOperator === '-'){
-    newResult = this.state.result - Number(this.state.inputValue)
+
+  plus = () => {
+    let newResult = this.state.result + Number(this.state.inputValue)
+    this.setState({
+      result: newResult,
+      operator: '+',
+      inputValue: ''
+    })
   }
-  this.setState({
-    storedValue: newResult,
-    operator: '+',
-    result: newResult,
-    inputValue: ''
-    
-  })
-}
 
-
-
-
-
-
-
-
-
-
-
-
-equalsTo = () => {
+  minus = () => {
+    let newResult
+    if (this.state.result === null) {
+      newResult = Number(this.state.inputValue)
+    }
+    else {
+      newResult = this.state.result - Number(this.state.inputValue)
+    }
+    this.setState({
+      result: newResult,
+      operator: '-',
+      inputValue: ''
+    })
+  }
   
-}
- 
-  render(){
-    console.log(`this is: operator=${this.state.operator}, inputValue=${this.state.inputValue}`);
-    console.log('state', this.state);
+  likamed = () => {
+    let newResult;
+    let input = Number(this.state.inputValue);
+    let latestOperator = this.state.operator
+    if (latestOperator === '+') {
+      newResult = this.state.result + input;
+    }
+    else if (latestOperator === '-') {
+      newResult = this.state.result - input;
+    }
+    this.setState({
+      storedValue: newResult,
+      result: newResult,
+      inputValue: ''
+    })
+  }
+
+  render() {
     return (
       <div className="App">
-      <input type="text" onChange={ event => this.changeInputValue(event)} value={this.state.inputValue} /><br/>
-      <button onClick={this.plus}>+</button>
-      <button onClick={this.minus}>-</button>
-      <button onClick={this.likamed}>=</button>
-      <button onClick={(e) => this.deleteRow(e)}>CE</button><br/>
-      <button onClick={this.plus}>m-</button><br/>
-      
-      <span>result:
-      result {this.state.result}
-      </span><br/>
+        <input type="text" onChange={event => this.changeInputValue(event)} value={this.state.inputValue} /><br />
+        <button onClick={this.plus}>+</button>
+        <button onClick={this.minus}>-</button>
+        <button onClick={this.likamed}>=</button>
+        <button onClick={(e) => this.deleteRow(e)}>CE</button><br />
 
-      <span> operator:
-      result{this.state.operator}
-      </span><br/>
+        <span>result: {this.state.result}
+        </span><br />
 
-      <span> inputvalue:
-      result{this.state.inputValue} 
-      </span>
-     
-      
-       
-    
+        <span> operator: {this.state.operator}
+        </span><br />
+
+        <span> inputvalue: {this.state.inputValue}
+        </span>
       </div>
     );
   }
 
 }
 
-
-
 export default App;
-
-
-//<Plus />
-
